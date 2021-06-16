d65e777b040380cf3650660dec0c06938d138e63
Joona Kytöniemi <jnkyto@utu.fi>
2021-06-16 14:48:04 +0300

    Modify GUI, lang files, bump dependencies

--- src/main/java/com/ahenkeshi/unikko/Unikko.java
+++ src/main/java/com/ahenkeshi/unikko/Unikko.java
@@ -14,7 +14,7 @@ public class Unikko implements ClientModInitializer, ModInitializer {
 
 	public static final String MODID = "Unikko";
 	public static final String VERSION = "1.0.0";
-	public static final String REL_DATE = "2021/06/07";
+	public static final String REL_DATE = "2021/06/16";
 
 	@Override
 	public void onInitialize()	{
